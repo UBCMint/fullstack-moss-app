05253351565d66b44c72b2acfca248644a46befd
= <hassanghayaty@outlook.com>
2024-10-26 21:09:03 -0700

    golang_simulator task 1 completed V1

--- golang_simulator/main.go
+++ golang_simulator/main.go
@@ -1,18 +1,78 @@
 package main
 
 import (
-	"fmt"      // This is used for formatting text and printing output
-	"net/http" // This allows us to create an HTTP server and define routes
+	"encoding/json" // Provides functions to encode/dencode data to/from JSON
+	"fmt"           // This is used for formatting text and printing output
+	"math/rand"     // Contains the methods to generate random numbers
+	"net/http"      // This allows us to create an HTTP server and define routes
+	"strconv"       // Converts strings to other types- useful got parsing query parameters
+	"time"          // Offers time-related functions - like pausing execution for intervals
 )
 
+var running bool = false // Defines a global variable that controls the state of the simulation - initialized as false until simulation starts
+var limit = 100          // Sets a default upper limit for the random numbers in the array
+
+// Generates a random array of length 64
+func generateData(limit int) []int {
+	data := make([]int, 64) // Initializes an array with 64 integers - all initially 0
+	// data = [0,0,0....] (length 64)
+	for i := range data {
+		data[i] = rand.Intn(limit + 1) // Assigns a random integer between 0 and the limit to each position in the array
+	}
+	return data
+}
+
 // Handles the /start endpoint to begin the simulation
+// r is an instance of http.Request, which contains details about the incoming HTTP request
 func startSimulation(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Simulation started")
+	if running {
+		fmt.Fprintln(w, "Simulation is already running!")
+		return
+	} else {
+		// Check for a limit parameter in the request query
+		// "r.URL.Query()["limit"]" Retrives any query parameter named "limit"
+		// "l" is assigned as the query paramter associated with "limit"
+		if l, ok := r.URL.Query()["limit"]; ok {
+			// "strconv.Atoi(l[0])" Converst the paramter value from a string to an integer (first item in l)
+			if val, err := strconv.Atoi(l[0]); err == nil {
+				running = true // Running variable is now set to true because the simulation is running
+				limit = val
+				fmt.Fprintln(w, "Simulation started with limit:", limit)
+			} else {
+				fmt.Fprintln(w, "Invalid limit value")
+				return
+			}
+		} else {
+			running = true // Running variable is now set to true because the simulation is running
+			fmt.Fprintln(w, "Simulation has started with default limit of 100")
+		}
+
+		// Start sending data as JSON arrays continuously
+		go func() {
+			for running {
+				data := generateData(limit)
+				jsonData, err := json.Marshal(data) // converts the data array to JSON format
+				// Marshalling is used for serialization - converts into a byte stream (JSON) that can be sent over the network
+				if err != nil {
+					fmt.Println("Error marshaling data:", err) // Error handling for JSON marshaling
+					continue
+				}
+				fmt.Println(string(jsonData))
+				time.Sleep(500 * time.Millisecond) // Pauses for 500 milliseconds between data sends
+			}
+		}()
+	}
 }
 
 // Handles the /end endpoint to end the simulation
 func endSimulation(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "Simulation ended")
+	if !running {
+		fmt.Fprintln(w, "Error! Simulation has not started to end")
+		return
+	} else {
+		running = false // Sets the running state to false
+		fmt.Fprintln(w, "Simulation ended")
+	}
 }
 
 func main() {
