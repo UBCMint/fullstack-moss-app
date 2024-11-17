89897a971a919a9cec1db1b2808d6c6684bf618b
jaydenpiao-ml <jayden.piao@marrlabs.com>
2024-11-16 22:04:24 -0800

    install sql plugin

--- mint-backend-tauri-app/src-tauri/src/main.rs
+++ mint-backend-tauri-app/src-tauri/src/main.rs
@@ -3,14 +3,15 @@
 
 #[tauri::command]
 fn greet(name: &str) -> String {
-  println!("inside rust code");
-  format!("hello {}!", name)
+    println!("inside rust code");
+    format!("hello {}!", name)
 }
 
 fn main() {
-  tauri::Builder::default()
-    // This is where you pass in your commands
-    .invoke_handler(tauri::generate_handler![greet])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        // This is where you pass in your commands
+        .invoke_handler(tauri::generate_handler![greet])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
