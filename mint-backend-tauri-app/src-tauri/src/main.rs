81f9faa8651d6b313cc8e58faee21166c1599197
jaydenpiao-ml <jayden.piao@marrlabs.com>
2024-11-16 23:16:32 -0800

    db initialize and add works

--- mint-backend-tauri-app/src-tauri/src/main.rs
+++ mint-backend-tauri-app/src-tauri/src/main.rs
@@ -4,6 +4,7 @@
 mod db;
 
 use tauri::State;
+use std::sync::Mutex;
 use db::{initialize_connection, AppState, initialize_db, add_user};
 
 #[tauri::command]
@@ -18,7 +19,7 @@ fn main() {
     tauri::Builder::default()
         // This is where you pass in your commands
         .manage(AppState {
-            conn: Mutext::new(conn),
+            conn: Mutex::new(conn),
         })
         .invoke_handler(tauri::generate_handler![greet, initialize_db, add_user])
         .run(tauri::generate_context!())
