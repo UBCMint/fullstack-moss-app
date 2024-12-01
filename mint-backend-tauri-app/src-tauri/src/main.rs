b52cef445417723d85127aca9ca16dd534e5591b
jaydenpiao-ml <jayden.piao@marrlabs.com>
2024-12-01 11:29:20 -0800

    add buttons to read/write random time series data

--- mint-backend-tauri-app/src-tauri/src/main.rs
+++ mint-backend-tauri-app/src-tauri/src/main.rs
@@ -5,7 +5,7 @@ mod db;
 
 use tauri::State;
 use std::sync::Mutex;
-use db::{initialize_connection, AppState, initialize_db, add_user, get_users};
+use db::{initialize_connection, AppState, initialize_db, add_user, get_users, add_time_series_data, get_time_series_data};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -21,7 +21,7 @@ fn main() {
         .manage(AppState {
             conn: Mutex::new(conn),
         })
-        .invoke_handler(tauri::generate_handler![greet, initialize_db, add_user, get_users])
+        .invoke_handler(tauri::generate_handler![greet, initialize_db, add_user, get_users, add_time_series_data, get_time_series_data])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
